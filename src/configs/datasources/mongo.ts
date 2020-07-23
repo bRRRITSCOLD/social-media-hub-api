52d2a77a3b605f0fec4223ddef94922ccdcc0da1
Blaine Richardson <blainerrichardson@gmail.com>
2020-07-22 20:54:15 -0500

    feat(mongodb) init of mongodb functionality

---
+++ src/configs/datasources/mongo.ts
@@ -0,0 +1,26 @@
+// libraries
+import { env } from "../../lib/environment";
+
+export default [
+  {
+      name: env.MONGO_SOCIAL_MEDIA_HUB_DB_NAME,
+      database: env.MONGO_SOCIAL_MEDIA_HUB_DB_NAME,
+      options: {
+        poolSize: 20,
+        authMechanism: 'DEFAULT',
+        authSource: 'admin',
+        auth: {
+          user: env.MONGO_SOCIAL_MEDIA_HUB_API_USER,
+          password: env.MONGO_SOCIAL_MEDIA_HUB_PI_PASSWORD
+        },
+        keepAlive: true,
+        connectTimeoutMS: 500000,
+        socketTimeoutMS: 500000,
+        reconnectTries: 3,
+        reconnectInterval: 500,
+      },
+      connectionStaleTimeframe: 15,
+      host: process.env.TEST_DB_MONGO_HOST,
+      port: process.env.TEST_DB_MONGO_PORT
+    }
+]
--- src/lib/environment/index.ts
+++ src/lib/environment/index.ts
@@ -4,6 +4,12 @@ export interface EnvInterface extends EnvironmentInterface {
   PORT: number;
   TIWTTER_CONSUMER_KEY: string;
   TIWTTER_CONSUMER_SECRET: string;
+  TIWTTER_CALLBACK_URL: string;
+  MONGO_SOCIAL_MEDIA_HUB_DB_HOST: string;
+  MONGO_SOCIAL_MEDIA_HUB_DB_NAME: string;
+  MONGO_SOCIAL_MEDIA_HUB_API_USER: string;
+  MONGO_SOCIAL_MEDIA_HUB_PI_PASSWORD: string;
+  MONGO_SOCIAL_MEDIA_HUB_TWITTER_CREDENTIALS_COLLECTION_NAME: string;
 }
 
 export class Env extends Environment implements EnvInterface {
@@ -16,6 +22,24 @@ export class Env extends Environment implements EnvInterface {
   }
   public get TIWTTER_CONSUMER_SECRET(): string {
     return process.env.TIWTTER_CONSUMER_SECRET as string;
+  }
+  public get TIWTTER_CALLBACK_URL(): string {
+    return process.env.TIWTTER_CALLBACK_URL as string;
+  }
+  public get MONGO_SOCIAL_MEDIA_HUB_DB_HOST(): string {
+    return process.env.MONGO_SOCIAL_MEDIA_HUB_DB_HOST as string;
+  }
+  public get MONGO_SOCIAL_MEDIA_HUB_DB_NAME(): string {
+    return process.env.MONGO_SOCIAL_MEDIA_HUB_DB_NAME as string;
+  }
+  public get MONGO_SOCIAL_MEDIA_HUB_API_USER(): string {
+    return process.env.MONGO_SOCIAL_MEDIA_HUB_API_USER as string;
+  }
+  public get MONGO_SOCIAL_MEDIA_HUB_PI_PASSWORD(): string {
+    return process.env.MONGO_SOCIAL_MEDIA_HUB_PI_PASSWORD as string;
+  }
+  public get MONGO_SOCIAL_MEDIA_HUB_TWITTER_CREDENTIALS_COLLECTION_NAME(): string {
+    return process.env.MONGO_SOCIAL_MEDIA_HUB_TWITTER_CREDENTIALS_COLLECTION_NAME as string;
   }
 
   // computed values
